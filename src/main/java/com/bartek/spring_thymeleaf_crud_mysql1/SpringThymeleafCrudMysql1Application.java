962253d71775cc1a1de1ccba32155a7e94698bd1
bartosz <bartoszklocek1985@gmail.com>
2020-08-11 18:21:34 +0200

    First commit

---
+++ src/main/java/com/bartek/spring_thymeleaf_crud_mysql1/SpringThymeleafCrudMysql1Application.java
@@ -0,0 +1,13 @@
+package com.bartek.spring_thymeleaf_crud_mysql1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafCrudMysql1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeleafCrudMysql1Application.class, args);
+    }
+
+}
